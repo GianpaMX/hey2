a2c1b6ff761d4547ddc17bafec09422b89907b48
Juan Villa <juan64@gmail.com>
2016-07-02 13:19:51 -0500

    Testing AddEditTaskPresenter

---
+++ androidapp/src/test/java/net/ddns/softux/hey/tests/androidapp/addedittask/AddEditTaskPresenterTest.java
@@ -0,0 +1,38 @@
+package net.ddns.softux.hey.tests.androidapp.addedittask;
+
+import net.ddns.softux.hey.androidapp.addedittask.AddEditTaskPresenter;
+import net.ddns.softux.hey.androidapp.addedittask.AddEditTaskView;
+import net.ddns.softux.hey.androidapp.addedittask.TaskViewModel;
+import net.ddns.softux.hey.todoapp.savetask.Task;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by juan on 2/07/16.
+ */
+public class AddEditTaskPresenterTest {
+    @Test
+    public void onSavedTask() throws Exception {
+        AddEditTaskView mockAddEditTaskView = mock(AddEditTaskView.class);
+
+        AddEditTaskPresenter addEditTaskPresenter = new AddEditTaskPresenter(mockAddEditTaskView);
+        addEditTaskPresenter.onSavedTask(new Task());
+
+        verify(mockAddEditTaskView).showSuccess(any(TaskViewModel.class));
+    }
+
+    @Test
+    public void viewGetterAndSetter() {
+        AddEditTaskView mockAddEditTaskView = mock(AddEditTaskView.class);
+        AddEditTaskPresenter addEditTaskPresenter = new AddEditTaskPresenter();
+
+        addEditTaskPresenter.setView(mockAddEditTaskView);
+
+        assertEquals(mockAddEditTaskView, addEditTaskPresenter.getView());
+    }
+}
